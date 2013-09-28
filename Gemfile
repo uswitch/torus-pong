37ea7e040ef5fed16f034cd21060ed2819580c4e
Thomas Kristensen <thomas.kristensen@uswitch.com>
2013-09-28 07:30:22 +0100

    Add Gemfile and Capify.

---
+++ Gemfile
@@ -0,0 +1,7 @@
+source "https://rubygems.org"
+
+gem 'bundler'
+
+group :development do
+  gem "capistrano", "2.9.0"
+end
---
+++ config/deploy.rb
@@ -0,0 +1,26 @@
+set :application, "torus-pong"
+set :user, "deploy"
+set :keep_releases, 3
+set :deploy_to,           "/var/www/#{application}"
+set :repository_cache,    "#{application}_cache"
+
+set :repository,  "https://deveo.com/clojurecup/projects/pong/repositories/git/torus-pong"
+set :scm, :git
+set :ssh_options, {:forward_agent => true}
+set :use_sudo, false
+set :deploy_via, :remote_cache
+set :scm_verbose, true
+set :normalize_asset_timestamps, false
+
+server 'pong.clojurecup.com', :app, :web, :db, :primary => true
+
+namespace :deploy do
+
+  desc "Build application as an uber jar"
+  task :build_uber_jar do
+    run("cd #{release_path}; lein uberjar")
+    run("mv #{release_path}/target/torus-pong.jar #{release_path}/.")
+  end
+
+  after "deploy:symlink", "deploy:build_uber_jar"
+end
