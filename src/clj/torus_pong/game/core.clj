1e6e7111b2e4c179269ce5fb748fd148d7dc70b6
Ragnar Dahlén <r.dahlen@gmail.com>
2013-09-28 08:20:48 +0100

    Add proposal for core game skeleton.

---
+++ src/clj/torus_pong/game/core.clj
@@ -0,0 +1,9 @@
+(ns torus-pong.game.core
+  (:require [torus-pong.game.params]))
+
+(defn advance
+  "Given a game-state and some inputs, advance the game-state one
+  tick"
+  [game-state inputs]
+  game-state)
+
---
+++ src/clj/torus_pong/game/params.clj
@@ -0,0 +1,5 @@
+(ns torus-pong.game.params)
+
+(def ticks-per-sec 10)
+
+(def tick-ms (/ 1000 ticks-per-sec))
