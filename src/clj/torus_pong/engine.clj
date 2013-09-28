771af06419019ebb7558137c347edb994e94f8a3
Ragnar Dahlén <r.dahlen@gmail.com>
2013-09-28 10:54:26 +0100

    Minor

--- src/clj/torus_pong/engine.clj
+++ src/clj/torus_pong/engine.clj
@@ -1,11 +1,12 @@
 (ns torus-pong.engine
-  (:require [clojure.core.async :refer [go alts! <! >! timeout]]))
+  (:require [clojure.core.async :refer [go alts! <! >! timeout]]
+            [torus-pong.game.params :as params]))
 
 (defn spawn-engine-process!
   [command-chan]
   (go (loop [game-state {}
              commands   []]
-        (let [[v c] (alts! [command-chan])]
+        (let [[v c] (alts! [command-chan ])]
           (condp = c
             command-chan (do (println "Got command" v)
                              (recur game-state commands)))))))
