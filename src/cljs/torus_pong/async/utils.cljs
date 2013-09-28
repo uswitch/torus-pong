158fa0958043e9ba422ca00ae3af31013d65370a
Ragnar Dahlén <r.dahlen@gmail.com>
2013-09-28 09:22:08 +0100

    Docstring for event-chan

--- src/cljs/torus_pong/async/utils.cljs
+++ src/cljs/torus_pong/async/utils.cljs
@@ -3,6 +3,10 @@
   (:require [cljs.core.async :refer [<! >! chan timeout put!]]))
 
 (defn event-chan
+  "Given an event type, and a function, return a channel where the
+  result of applying f to every event of the given type is put when
+  the event occurs. Optionally specify an element to limit event
+  scope, and an existing channel, where output should be put."
   ([type f] (event-chan js/window type f))
   ([el type f] (event-chan (chan) el type f))
   ([c el type f]
