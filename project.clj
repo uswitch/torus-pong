0711f3d0b1501d342421542f44e1ba5423c1449e
Ragnar Dahlén <r.dahlen@gmail.com>
2013-09-28 06:26:23 +0100

    Add core.async staging repo

--- project.clj
+++ project.clj
@@ -3,6 +3,8 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :repositories {"sonatype-staging"
+                 "https://oss.sonatype.org/content/groups/staging/"}
   :dependencies [[org.clojure/clojure "1.5.1"]
                  [org.clojure/clojurescript "0.0-1909"]
                  [core.async "0.1.0-SNAPSHOT"]
