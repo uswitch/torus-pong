a2282c7559d335dc1c0cce4d6005595af638408f
Thomas Kristensen <thomas.kristensen@uswitch.com>
2013-09-29 11:38:46 +0100

    Force tag.

--- config/deploy.rb
+++ config/deploy.rb
@@ -74,7 +74,7 @@ namespace :tagging do
   task :push_deploy_tag do
     user = `git config --get user.name`.chomp
     email = `git config --get user.email`.chomp
-    puts `git tag "clojurecup13" #{current_revision} -m "Deployed by #{user} <#{email}>"`
+    puts `git tag -f "clojurecup13" #{current_revision} -m "Deployed by #{user} <#{email}>"`
     puts `git push --tags origin`
   end
 end
