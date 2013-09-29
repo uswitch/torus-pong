821cedf436337ee15988f2e8a98a73bc2d6843f0
Ragnar Dahlén <r.dahlen@gmail.com>
2013-09-29 11:55:08 +0100

    Remove more just listen stuff.

--- src/clj/torus_pong/server.clj
+++ src/clj/torus_pong/server.clj
@@ -35,7 +35,7 @@
      (println "Client process terminating"))))
 
 (defn spawn-connection-process!
-  [conn-chan command-chan clients listen-clients]
+  [conn-chan command-chan clients]
   (go (loop [{:keys [request in out] :as conn} (<! conn-chan)]
         (when conn
           (let [id (next-id)]
